3f058203b11cb4c94f04938ae65fd4647b16246e
Daniela Martínez <danielamartinez@Danielas-MacBook-Pro.local>
2021-10-01 23:20:08 -0300

    images

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Login from './pages/login/login';
 import Weather from './pages/weather/weather';
 
@@ -7,7 +7,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Redirect to="/login" />
         <Route exact path="/pronostico-por-region" component={Weather} />
       </div>
     </Router>
